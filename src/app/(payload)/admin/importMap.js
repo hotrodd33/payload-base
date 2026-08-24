c3ee8d0e5e4e6b3c1150e18c1a215394d8b62006
Isaac Corey <icorey@digiseeit.com>
2026-08-23 20:13:23 -0500

    Updates after install

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -27,6 +27,7 @@ import { ColumnRowLabel as ColumnRowLabel_1ccbee42583546acda61048c8bda4d8e } fro
 import { SocialLinkRowLabel as SocialLinkRowLabel_9697010365a26e30d4bc253bddcd7804 } from '@/Footer/SocialLinkRowLabel'
 import { default as default_1a7510af427896d367a49dbf838d2de6 } from '@/components/BeforeDashboard'
 import { default as default_8a7ab0eb7ab5c511aba12e68480bfe5e } from '@/components/BeforeLogin'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 /** @type import('payload').ImportMap */
@@ -60,5 +61,6 @@ export const importMap = {
   "@/Footer/SocialLinkRowLabel#SocialLinkRowLabel": SocialLinkRowLabel_9697010365a26e30d4bc253bddcd7804,
   "@/components/BeforeDashboard#default": default_1a7510af427896d367a49dbf838d2de6,
   "@/components/BeforeLogin#default": default_8a7ab0eb7ab5c511aba12e68480bfe5e,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
